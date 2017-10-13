e9c954acae21f1127619a119d88a3cd23955aa87
rheidi <32763180+rheidi@users.noreply.github.com>
2017-10-13 12:00:48 +0300

    Create Raaka_aine.java

---
+++ src/main/java/tikape/runko/domain/Raaka_aine.java
@@ -0,0 +1,29 @@
+package tikape.runko.domain;
+
+public class Raaka_aine {
+    
+    private Integer id;
+    private String nimi;
+    
+    public Raaka_aine(Integer id, String nimi) {
+        this.id = id;
+        this.nimi = nimi;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+    
+}
