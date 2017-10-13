5ba8c39c9b1fd0c02913b900869143dc64ec433c
Mirex97 <32763253+Mirex97@users.noreply.github.com>
2017-10-13 12:04:02 +0300

    Update Annos.java

--- src/main/java/tikape/runko/domain/Annos.java
+++ src/main/java/tikape/runko/domain/Annos.java
@@ -1,1 +1,31 @@
 package tikape.runko.domain;
+
+public class Annos {
+    private Integer id;
+    private String nimi;
+    private String ohje;
+    private Integer maara;
+    
+    public Annos(int id, String nimi, String ohje, int maara) {
+        this.id = id;
+        this.nimi = nimi;
+        this.ohje = ohje;
+        this.maara = maara;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public String getOhje() {
+        return ohje;
+    }
+
+    public Integer getMaara() {
+        return maara;
+    }
+}
