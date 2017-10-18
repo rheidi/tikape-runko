6136302b66d7cc7a20496caa57dda5ce740f9d14
Rex <Mirex97@gmail.com>
2017-10-18 05:00:21 +0300

    Tein nyt noi html:ät, mutta ne vielä tarvitsevat hieman muokkaamista.
    Signed-off-by: Rex <Mirex97@gmail.com>

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -5,7 +5,6 @@ import spark.ModelAndView;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 import tikape.runko.database.Database;
-import tikape.runko.database.OpiskelijaDao;
 
 public class Main {
 
@@ -13,7 +12,6 @@ public class Main {
         Database database = new Database("jdbc:sqlite:opiskelijat.db");
         database.init();
 
-        OpiskelijaDao opiskelijaDao = new OpiskelijaDao(database);
 
         get("/", (req, res) -> {
             HashMap map = new HashMap<>();
@@ -24,14 +22,12 @@ public class Main {
 
         get("/opiskelijat", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("opiskelijat", opiskelijaDao.findAll());
 
             return new ModelAndView(map, "opiskelijat");
         }, new ThymeleafTemplateEngine());
 
         get("/opiskelijat/:id", (req, res) -> {
             HashMap map = new HashMap<>();
-            map.put("opiskelija", opiskelijaDao.findOne(Integer.parseInt(req.params("id"))));
 
             return new ModelAndView(map, "opiskelija");
         }, new ThymeleafTemplateEngine());
